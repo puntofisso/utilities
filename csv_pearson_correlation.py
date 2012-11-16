d34741a848ddd77c2339a5ca0c591301add28ae3
Giuseppe Sollazzo <puntofisso@gmail.com>
2012-11-16 16:58:14 +0000

    Added guidelines and scipy comparison

--- csv_pearson_correlation.py
+++ csv_pearson_correlation.py
@@ -1,6 +1,14 @@
 import csv
 from itertools import imap
 import math
+from scipy.stats.stats import pearsonr
+
+
+#Correlation 	Negative 	Positive
+#None 		-0.09 to 0.0 	0.0 to 0.09
+#Small 		-0.3 to -0.1 	0.1 to 0.3
+#Medium 	-0.5 to -0.3 	0.3 to 0.5
+#Strong 	-1.0 to -0.5 	0.5 to 1.0
 
 def average(x):
     assert len(x) > 0
@@ -36,7 +44,30 @@ def main():
            ident.append(row[0])
            x.append(int(row[1]))
            y.append(int(row[2]))
-        print "Calculating Pearson Correlation..."
-        print str(pearson_def(x, y))
+        print "Calculating Pearson Correlation manually..."
+        res = pearson_def(x, y)
+        print res
+
+        if (res > 0.5):
+            print "STRONG (POSITIVE)"
+        elif (res > 0.3):
+            print "MEDIUM (POSITIVE)"
+        elif (res > 0.1):
+            print "SMALL (POSITIVE)"
+        elif (res > 0):
+            print "NONE (POSITIVE)"
+        elif (res > 0.1):
+            print "NONE (NEGATIVE)"
+        elif (res > 0.3):
+            print "SMALL (NEGATIVE)" 
+        elif (res > 0.5):
+            print "MEDIUM (NEGATIVE)"
+        elif (res > -1.0):
+            print "STRONG (NEGATIVE)"
+           
+
+        print "Calculating Pearson Correlation with scipy..."
+        res = pearsonr(x, y)
+        print res[0]
 
 main()
